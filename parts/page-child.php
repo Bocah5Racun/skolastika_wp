11ea167e3e74c739d5753ec1b47d8090c17cb87c
Bocah5Racun <onlymoderatelymad@gmail.com>
2024-04-17 10:40:30 +0800

    Added -child and -parent templates for single pages.

---
+++ parts/page-child.php
@@ -0,0 +1,57 @@
+<?php 
+    $study_program = get_the_title( wp_get_post_parent_id() );
+?>
+
+<div id="single-hero-container">
+    <div class="single-hero-title-container container">
+        <a href=".."><h1><?= $study_program ?></h1></a>
+    </div>
+    <img src="<?= get_the_post_thumbnail_url( wp_get_post_parent_id( get_the_ID() )); ?>" />
+    <div class="gradient--radial"></div>
+</div>
+
+<section id="study-program-info">
+    <div class="study-program-info-inner container">
+        <div class="program-studi-info-list">
+        <?php
+                $args = array(
+                    'post_type'     => 'page',
+                    'post_parent'   => wp_get_post_parent_id( get_the_ID() ),
+                );
+                
+                $query = new WP_Query( $args );
+
+                if( $query->have_posts() ):
+                    while( $query->have_posts() ):
+                        $query->the_post();
+            ?>
+
+            <a href="<?= get_the_permalink() . "#title"; ?>"><?= get_the_title(); ?></a>
+
+            <?php
+                endwhile;
+                endif;
+                wp_reset_postdata();
+            ?>
+        </div>
+    </div>
+</section>
+
+<section class="section">
+    <div class="section-inner container">
+        <article class="centered-box">
+            <h1 id="title"><?= get_the_title(); ?></h1>
+            <div class="last-updated">Laman ini terakhir diperbarui pada tanggal <?= get_the_modified_date(); ?>.</div>
+            <hr class="separator--blue" />
+            <div class="constrained">
+                <?= the_content(); ?>
+            </div>
+        </article>
+        <div class="section section--upri-yellow study-program-child-cta-container p-4">
+            <div class="section-inner">
+                <h2>Gabung bersama kami membuat perubahan yang berdampak.</h2>
+                <a class="single-cta-button" href="<?= get_home_url() . '/penerimaan-mahasiswa-baru/'; ?>">Baca informasi pendaftaran mahasiswa baru →</a>
+            </div>
+        </div>
+    </div>
+</section>
