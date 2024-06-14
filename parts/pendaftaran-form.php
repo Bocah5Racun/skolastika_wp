c41ddf88a59a9301993c03088c876fbd1126d05e
Bocah5Racun <onlymoderatelymad@gmail.com>
2024-06-14 08:28:19 +0800

    More tweaking registration funnel.

--- parts/pendaftaran-form.php
+++ parts/pendaftaran-form.php
@@ -54,83 +54,80 @@
     </section>
 
     <?php else: ?>
-
-
-    <form id="register-form" name="register_form" class="centered-box constrained"  method="post">
-        <div>
-            <h2>Mulai Proses Registrasi</h2>
-            <p>Isi formulir di bawah ini untuk memulai proses registrasi sebagai calon mahasiswa baru di Fakultas Ilmu Sosial dan Ilmu Politik di Universitas Pejuang Republik Indonesia.</p>
-            <p><b>Tidak ada pemungutan biaya untuk tahap ini.</b></p>
-        </div>
-        <div class="form-section">
-            <h3>Jenis Pendaftaran</h3>
-            <div class="form-row">
-                <div class="form-radio-option">
-                    <input type="radio" id="reguler" value="reguler" name="register_program" />
-                    <label for="reguler">Reguler</label>
-                </div>
-                <div class="form-radio-option">
-                    <input type="radio" id="rpl" value="rpl" name="register_program" />
-                    <label for="rpl">
-                    Rekognisi Pembelajaran Lampau (RPL) (<a href="https://fisipupri.ac.id/penerimaan-mahasiswa-baru/program-rekognisi-pembalajaran-lampau-rpl/">Lihat Detail →</a>)
-                    </label>
-                </div>
-            </div>
-        </div>
-        <div class="form-section">
-            <h3>Program Studi Pilihan</h3>
-            <div class="form-row">
-                <div class="form-radio-option">
-                    <input type="radio" id="komunikasi" value="komunikasi" name="register_department" required />
-                    <label for="komunikasi">
-                    Ilmu Komunikasi (<a href="https://fisipupri.ac.id/study-program-about/ilmu-komunikasi/">Lihat Detail →</a>)
-                    </label>
-                </div>
-                <div class="form-radio-option">
-                    <input type="radio" id="administrasi" value="komunikasi" name="register_department" />
-                    <label for="administrasi">
-                        Ilmu Administrasi Negara (<a href="https://fisipupri.ac.id/study-program-about/ilmu-administrasi-negara/">Lihat Detail →</a>)
-                    </label>
-                </div>
-            </div>
-        </div>
-        <div class="form-section">
-            <h3>Data Personal</h3>
-            <div class="form-row">
-                <label for="register_name">Nama Lengkap</label>
-                <input type="text" name="register_name" id="register_name" required />
-            </div>
-            <div class="form-row">
-                <label for="register_email">Email</label>
-                <input type="email" name="register_email" id="register_email" required />
-            </div>
-            <div class="form-row">
-                <label for="register_school">Sekolah Asal (yang mengeluarkan Ijazah SMA/sederajat)</label>
-                <input type="text" name="register_school" id="register_school" required />
-            </div>
-            <div class="form-row">
-                <label for="register_phone">Nomor Telepon/WhatsApp*</label>
-                <input type="tel" name="register_phone" id="register_phone" placeholder="+6285999999999" required />
-                <small>*nomor WhatsApp yang aktif.</small>
-            </div>
-        </div>
-        <button type="submit" class="g-recaptcha button button--blue" 
-        data-sitekey="6LcVlcQpAAAAANAtJ4wpBIswn4Cxn7Iiic1Xk09t" 
-        data-callback='onSubmit'
-        data-action='submit'>Mulai Proses Registrasi →</button>
-    </form>
-
     
     <div class="section-inner container">
         <article class="centered-box constrained">
             <h1 id="title"><?= get_the_title(); ?></h1>
             <div class="last-updated">Laman ini terakhir diperbarui pada tanggal <?= get_the_modified_date(); ?>.</div>
             <hr class="separator--blue" />
+            <form id="register-form" name="register_form" class="centered-box constrained"  method="post">
+                <div>
+                    <h2>Mulai Proses Registrasi</h2>
+                    <p>Isi formulir di bawah ini untuk memulai proses registrasi sebagai calon mahasiswa baru di Fakultas Ilmu Sosial dan Ilmu Politik di Universitas Pejuang Republik Indonesia.</p>
+                    <p><b>Tidak ada pemungutan biaya untuk tahap ini.</b></p>
+                </div>
+                <div class="form-section">
+                    <h3>Jenis Pendaftaran</h3>
+                    <div class="form-row">
+                        <div class="form-radio-option">
+                            <input type="radio" id="reguler" value="reguler" name="register_program" />
+                            <label for="reguler">Reguler</label>
+                        </div>
+                        <div class="form-radio-option">
+                            <input type="radio" id="rpl" value="rpl" name="register_program" />
+                            <label for="rpl">
+                            Rekognisi Pembelajaran Lampau (RPL) (<a href="https://fisipupri.ac.id/penerimaan-mahasiswa-baru/program-rekognisi-pembalajaran-lampau-rpl/">Lihat Detail →</a>)
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-section">
+                    <h3>Program Studi Pilihan</h3>
+                    <div class="form-row">
+                        <div class="form-radio-option">
+                            <input type="radio" id="komunikasi" value="komunikasi" name="register_department" required />
+                            <label for="komunikasi">
+                            Ilmu Komunikasi (<a href="https://fisipupri.ac.id/study-program-about/ilmu-komunikasi/">Lihat Detail →</a>)
+                            </label>
+                        </div>
+                        <div class="form-radio-option">
+                            <input type="radio" id="administrasi" value="komunikasi" name="register_department" />
+                            <label for="administrasi">
+                                Ilmu Administrasi Negara (<a href="https://fisipupri.ac.id/study-program-about/ilmu-administrasi-negara/">Lihat Detail →</a>)
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-section">
+                    <h3>Data Personal</h3>
+                    <div class="form-row">
+                        <label for="register_name">Nama Lengkap</label>
+                        <input type="text" name="register_name" id="register_name" required />
+                    </div>
+                    <div class="form-row">
+                        <label for="register_email">Email</label>
+                        <input type="email" name="register_email" id="register_email" required />
+                    </div>
+                    <div class="form-row">
+                        <label for="register_school">Sekolah Asal (yang mengeluarkan Ijazah SMA/sederajat)</label>
+                        <input type="text" name="register_school" id="register_school" required />
+                    </div>
+                    <div class="form-row">
+                        <label for="register_phone">Nomor Telepon/WhatsApp*</label>
+                        <input type="tel" name="register_phone" id="register_phone" placeholder="+6285999999999" required />
+                        <small>*nomor WhatsApp yang aktif.</small>
+                    </div>
+                </div>
+                <button type="submit" class="g-recaptcha button button--blue" 
+                data-sitekey="6LcVlcQpAAAAANAtJ4wpBIswn4Cxn7Iiic1Xk09t" 
+                data-callback='onSubmit'
+                data-action='submit'>Mulai Proses Registrasi →</button>
+            </form>
             <div class="constrained">
                 <?= the_content(); ?>
             </div>
         </article>
     </div>
-    
+
 <?php endif; ?>
 </section>
