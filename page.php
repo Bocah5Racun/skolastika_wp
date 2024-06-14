ab04ba8d072c50b6b299487540dfec65fc4acbd9
Bocah5Racun <onlymoderatelymad@gmail.com>
2024-06-14 11:39:52 +0800

    Child pages will scroll to title automatically.

--- page.php
+++ page.php
@@ -2,7 +2,16 @@
 
 <?php
     if( !get_post_parent() ) get_template_part( 'parts/page', 'parent');
-    if( get_post_parent() ) get_template_part( 'parts/page', 'child' );
+    if( get_post_parent() ) :
+        get_template_part( 'parts/page', 'child' );
 ?>
+<script>
+    document.onload( () => {
+        document.querySelector("#title").scrollIntoView({
+            behavior: 'smooth',
+        })
+    })
+</script>
+<?php endif; ?>
 
 <?php get_footer(); ?>
