e131a692ae63271161f2cad7bbb338551cb69e3a
Bocah5Racun <onlymoderatelymad@gmail.com>
2024-06-18 23:02:20 +0800

    Tweaked highlight block.

--- includes/plugins/skolastika-gutenberg-blocks/highlight-block.js
+++ includes/plugins/skolastika-gutenberg-blocks/highlight-block.js
@@ -8,25 +8,39 @@ registerBlockType("skolastika/highlight", {
   icon: "smiley",
   category: "common",
   attributes: {
+    title: { type: "string" },
     content: { type: "string" },
   },
   description: "Block for highlighting specific text.",
   edit: (props) => {
     const updateContent = (highlightText) =>
       props.setAttributes({ content: highlightText });
+    const updateTitle = (event) =>
+      props.setAttributes({ title: event.target.value });
     return ReactElement(
       "div",
       { className: "highlight" },
+      ReactElement("input", {
+        type: "text",
+        style: {
+          fontWeight: 700,
+        },
+        value: props.attributes.title,
+        placeholder: "The title of the highlight.",
+        onChange: updateTitle,
+      }),
       ReactElement(RichText, {
         value: props.attributes.content,
+        placeholder: "The body text of the highlight.",
         onChange: updateContent,
       })
     );
   },
   save: (props) => {
     return wpElement(
-      "p",
+      "div",
       { className: "highlight" },
+      props.attributes.title && wpElement("h3", null, props.attributes.title),
       ReactElement(RichText.Content, {
         value: props.attributes.content,
       })
