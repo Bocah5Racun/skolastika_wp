2c3f6b61df48451d0045a1ce2db2c054c37230a2
Bocah5Racun <onlymoderatelymad@gmail.com>
2025-02-20 23:45:02 +0800

    Major tweaks to the single page layout.

--- single.php
+++ single.php
@@ -46,28 +46,20 @@
 
 <article class="container section">
     <div class="section-inner">
-        <h1><?= get_the_title(); ?></h1>
-        <div class="constrained centered-box">
+        <div class="centered-box">
             <?php if( has_post_thumbnail() ) : ?>
             <img class="post-thumbnail" src="<?= get_the_post_thumbnail_url( get_the_ID(), "full" ); ?>" />
             <?php if( get_the_post_thumbnail_caption() ) : ?>
                 <div class="post-caption"><?= get_the_post_thumbnail_caption(); ?></div>
             <?php endif; ?>
             <?php endif; ?>
-            <div class="last-updated">Laman ini terakhir diperbarui pada tanggal <?= get_the_modified_date(); ?>.</div>
+            <div class="article-meta">
+                <h1><?= get_the_title(); ?></h1>
+                <div class="article-author">Oleh <?= get_the_author_meta( 'display_name', get_post_field( 'post_author' ) ); ?> <span class="article-author-separator">|</span> <?= get_the_date(); ?></div>
+            </div>
         </div>
         <div class="article-content constrained centered-box">
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4861861095469626"
-                    crossorigin="anonymous"></script>
-            <ins class="adsbygoogle"
-                style="display:block; text-align:center;"
-                data-ad-layout="in-article"
-                data-ad-format="fluid"
-                data-ad-client="ca-pub-4861861095469626"
-                data-ad-slot="7043857511"></ins>
-            <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
+            <div class="last-updated">Laman ini terakhir diperbarui pada tanggal <?= get_the_modified_date(); ?>.</div>
             <?= $the_content; ?>
         </div>
     </div>
