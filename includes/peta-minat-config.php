371b5e2d8fb2e723b713890b2a82a7c5815cf2e8
Bocah5Racun <onlymoderatelymad@gmail.com>
2025-07-14 13:32:43 +0800

    Added config file

---
+++ includes/peta-minat-config.php
@@ -0,0 +1,4 @@
+<?php
+
+define( 'PM_URL', 'https://script.google.com/macros/s/AKfycbxCctR7wiKAV4seJ9iJuyYjfhIBF_BDKoLIbL2z5J0fgmAJfYB1PEJ_kds1_Z6uFs0A/exec' );
+define( 'PM_API', 'K3VBLVZ3QV38LRUQ6N1G181R8KVJJNOH' );
