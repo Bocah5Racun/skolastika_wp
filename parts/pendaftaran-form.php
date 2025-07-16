64a72f70ac1457bb187cb9445d85554feedeccc3
Bocah5Racun <onlymoderatelymad@gmail.com>
2025-07-16 22:20:06 +0800

    Fixed selectors for loader

--- parts/pendaftaran-form.php
+++ parts/pendaftaran-form.php
@@ -134,7 +134,7 @@
                     </div>
                 </div>
                 <button id="reg-submit" type="submit" class="g-recaptcha button button--blue" data-sitekey="6LcVlcQpAAAAANAtJ4wpBIswn4Cxn7Iiic1Xk09t" data-callback="onSubmit" data-action="submit">Submit →</button>
-                <div class="reg-loader">⏳ Loading...</div>
+                <div id="reg-loader">⏳ Loading...</div>
             </form>
 .        </article>
     </div>
