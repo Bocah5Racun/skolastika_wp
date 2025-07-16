3c2d813e6a551b55f0a4b6df4a68185af6e39d99
Bocah5Racun <onlymoderatelymad@gmail.com>
2025-07-16 22:00:23 +0800

    Only run submission script if form is valid

--- includes/scripts/captcha-client.js
+++ includes/scripts/captcha-client.js
@@ -2,17 +2,17 @@ function onSubmit(token) {
   const button = document.getElementById("reg-submit");
   const form = document.getElementById("register-form");
 
-  //Disable button
-  button.textContent = "Loading...";
-  button.disabled = true;
-
-  setTimeout(() => {
-    button.textContent = "Submit →";
-    button.disabled = false;
-  }, 30000);
-
   //Check form validity
   if (form.checkValidity()) {
+    //Disable button
+    button.textContent = "Loading...";
+    button.disabled = true;
+
+    setTimeout(() => {
+      button.textContent = "Submit →";
+      button.disabled = false;
+    }, 30000);
+
     form.submit();
   } else {
     grecaptcha.reset();
