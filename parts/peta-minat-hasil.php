ff48cc668c998497424d1938eae07d3264f66489
Bocah5Racun <onlymoderatelymad@gmail.com>
2026-01-26 21:30:32 +0800

    Fixed  sometimes not being set.

--- parts/peta-minat-hasil.php
+++ parts/peta-minat-hasil.php
@@ -20,7 +20,6 @@ if( isset( $_POST['dimensions'] ) ) {
         $sekolah = isset( $_POST['sekolah'] ) ? filter_var( $_POST['sekolah'], FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH) : "-";
         $nomor = isset( $_POST['nomor'] ) ? preg_replace('/[^\d]/', '', $_POST['nomor']) : die( "Number not set" );
         $promo_bool = isset( $_POST['promo_bool'] ) ? $_POST['promo_bool'] : false;
-        $admin_view = isset( $_GET['role'] ) ? '1' : false; // to show admin view
     }
 
     $ratings = json_decode( stripslashes( $_POST['dimensions'] ), true );
@@ -34,6 +33,8 @@ if( isset( $_POST['dimensions'] ) ) {
         'E' => 0,
         'C' => 0,
     ];
+    
+    $admin_view = isset( $_GET['role'] ) ? '1' : false; // to show admin view
     
     foreach( $ratings as $rating ) {
     
